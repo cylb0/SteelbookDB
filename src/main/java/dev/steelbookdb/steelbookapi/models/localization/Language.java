e1a4ef89f2afaebc2072c7cd20900d4d07971bcb
cylb0 <foucaut.morgan@gmail.com>
2025-08-07 17:57:02 +0200

    feat(models): establish many-to-many relationship between Disk and Language for subtitle support

--- src/main/java/dev/steelbookdb/steelbookapi/models/localization/Language.java
+++ src/main/java/dev/steelbookdb/steelbookapi/models/localization/Language.java
@@ -4,7 +4,9 @@ import java.util.Set;
 
 import dev.steelbookdb.steelbookapi.models.BaseEntity;
 import dev.steelbookdb.steelbookapi.models.movie.MovieTranslation;
+import dev.steelbookdb.steelbookapi.models.steelbook.Disk;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -25,5 +27,8 @@ public class Language extends BaseEntity {
 
     @OneToMany(mappedBy = "language")
     private Set<MovieTranslation> movieTranslations;
+
+    @ManyToMany(mappedBy = "subtitleLanguages")
+    private Set<Disk> disksWithSubDisks;
 
 }
--- src/main/java/dev/steelbookdb/steelbookapi/models/steelbook/Disk.java
+++ src/main/java/dev/steelbookdb/steelbookapi/models/steelbook/Disk.java
@@ -3,9 +3,11 @@ package dev.steelbookdb.steelbookapi.models.steelbook;
 import java.util.Set;
 
 import dev.steelbookdb.steelbookapi.models.BaseEntity;
+import dev.steelbookdb.steelbookapi.models.localization.Language;
 import dev.steelbookdb.steelbookapi.models.movie.Movie;
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -31,4 +33,12 @@ public class Disk extends BaseEntity {
     @ManyToOne
     @JoinColumn(name = "movie_id")
     private Movie movie;
+
+    @ManyToMany
+    @JoinTable(
+        name = "disk_subtitles",
+        joinColumns = @JoinColumn(name = "disk_id"),
+        inverseJoinColumns = @JoinColumn(name = "language_id")
+    )
+    private Set<Language> subtitleLanguages;
 }
