d139e57f812d2235da5cc96335f21263f0288278
cylb0 <foucaut.morgan@gmail.com>
2025-08-08 12:47:24 +0200

    feat(bluray): add BlurayRegion enum with defined regions feat(dvd): add DvdRegion enum with defined regions

---
+++ src/main/java/dev/steelbookdb/steelbookapi/steelbook/BlurayRegion.java
@@ -0,0 +1,8 @@
+package dev.steelbookdb.steelbookapi.steelbook;
+
+public enum BlurayRegion {
+    REGION_A,
+    REGION_B,
+    REGION_C,
+    REGION_FREE
+}
---
+++ src/main/java/dev/steelbookdb/steelbookapi/steelbook/DvdRegion.java
@@ -0,0 +1,13 @@
+package dev.steelbookdb.steelbookapi.steelbook;
+
+public enum DvdRegion {
+    REGION_0,
+    REGION_1,
+    REGION_2,
+    REGION_3,
+    REGION_4,
+    REGION_5,
+    REGION_6,
+    REGION_7,
+    REGION_8,
+}
