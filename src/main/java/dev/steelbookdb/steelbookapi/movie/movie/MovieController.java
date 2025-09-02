7c51b93c0e256cb747a26c7fd4546805cc75b65a
cylb0 <foucaut.morgan@gmail.com>
2025-09-02 09:58:32 +0200

    feat(controller): add MovieController with saveMovie and getAllMovies endpoints

---
+++ src/main/java/dev/steelbookdb/steelbookapi/movie/movie/MovieController.java
@@ -0,0 +1,31 @@
+package dev.steelbookdb.steelbookapi.movie.movie;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import dev.steelbookdb.steelbookapi.movie.movie.dto.CreateMovieDto;
+import dev.steelbookdb.steelbookapi.movie.movie.dto.MovieDto;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class MovieController {
+
+    private final MovieService movieService;
+
+    @PostMapping("/movies")
+    public MovieDto saveMovie(@Valid @RequestBody CreateMovieDto dto) {
+        return movieService.createMovie(dto); 
+    }
+
+    @GetMapping("/movies")
+    public List<MovieDto> getAllMovies() {
+        return movieService.getAllMovies();
+    }
+    
+}
